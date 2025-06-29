67940cae142d8b52cd71284c67f6ec4c5e914ec2
Rh4096 <rhelvetican@gmail.com>
2025-06-30 00:46:05 +0700

    oops

--- items/Colors.lua
+++ items/Colors.lua
@@ -5,17 +5,17 @@ Arklatro.gradient.mythic = SMODS.Gradient({
 	key = "mythic_gradient",
 
 	colours = {
-		HEX("#95ECB0"),
-		HEX("#F3F98A"),
+		HEX("95ECB0"),
+		HEX("F3F98A"),
 	},
 })
 
 Arklatro.gradient.ordinal = SMODS.Gradient({
 	key = "ordinal_gradient",
 	colours = {
-		HEX("#84FFC9"),
-		HEX("#AAB2FF"),
-		HEX("#ECA0FF"),
-		HEX("#AAB2FF"),
+		HEX("84FFC9"),
+		HEX("AAB2FF"),
+		HEX("ECA0FF"),
+		HEX("AAB2FF"),
 	},
 })
